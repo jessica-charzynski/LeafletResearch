9d314bdd9c8e2a987028a0b900e988517d9e9548
jessica-charzynski <jessica.charzynski@web.de>
2024-10-17 15:06:13 +0200

    Integration of Spring Boot Framework

---
+++ src/main/java/com/paf/leafletresearch/LeafletResearchApplication.java
@@ -0,0 +1,12 @@
+package com.paf.leafletresearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeafletResearchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LeafletResearchApplication.class, args);
+        System.out.println("Spring Boot application started successfully! :)");
+    }
+}
